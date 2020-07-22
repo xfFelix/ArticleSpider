6eac05cce33117a5a202d3d5c660ff45474ba2e6
xfFelix <13590344852@163.com>
2020-07-22 23:14:24 +0800

    first commit

---
+++ ArticleSpider/spiders/jobbole.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class JobboleSpider(scrapy.Spider):
+    name = 'jobbole'
+    allowed_domains = ['blog.jobbole.com']
+    start_urls = ['http://blog.jobbole.com/']
+
+    def parse(self, response):
+        pass
